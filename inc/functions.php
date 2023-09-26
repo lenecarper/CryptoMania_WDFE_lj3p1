9a7e221f0c56634acafe87ea24859ffa96601dd0
Lene <lenecarpertunnel@gmail.com>
2023-09-26 14:03:22 +0200

    PHP register script

---
+++ inc/functions.php
@@ -0,0 +1,57 @@
+<?php
+function registerUser($username, $password)
+{
+    // Connect to the MySQL database
+    $db = new mysqli('localhost', 'root', '', 'cryptomania');
+
+    // Check the connection
+    if ($db->connect_errno) {
+        return "Connection failed: " . $db->connect_error;
+    }
+
+    // Check if the username already exists in the database
+    $checkQuery = $db->prepare("SELECT COUNT(*) FROM users WHERE username = ?");
+    $checkQuery->bind_param("s", $username);
+    $checkQuery->execute();
+    $checkQuery->bind_result($count);
+    $checkQuery->fetch();
+    $checkQuery->close();
+
+    if ($count > 0)
+    {
+        return "Username already exists. Please choose a different username.";
+    }
+
+    // Hash the password before storing it in the database
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Insert the user into the database
+    $insertQuery = $db->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $insertQuery->bind_param("ss", $username, $hashedPassword);
+
+    if ($insertQuery->execute())
+    {
+        $insertQuery->close();
+        $db->close();
+        return "User registered successfully.";
+    }
+    else
+    {
+        $insertQuery->close();
+        $db->close();
+        return "Error registering user: " . $db->error;
+    }
+}
+
+if (isset($_POST['submit-register'])) {
+    $username = $_POST['username-register'];
+    $password = $_POST['password-register'];
+
+    // Call the registerUser function with the form data
+    $registrationResult = registerUser($username, $password);
+
+    // Display the result of the registration attempt
+    echo $registrationResult;
+}
+
+?>
